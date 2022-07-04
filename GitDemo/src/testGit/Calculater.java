047a0fd7ee12dfc2d9a25dc6bac6bd4bb74174ca
VimeeBakori <vimeebakori@gmail.com>
2022-07-04 12:30:49 +0200

    Changes made by tester

--- GitDemo/src/testGit/Calculater.java
+++ GitDemo/src/testGit/Calculater.java
@@ -2,11 +2,37 @@ package testGit;
 
 public class Calculater {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Calculater test= new Calculater();
-		 int sum=Add.add(10,20);
-		 System.out.println(sum);
+	public void addTest()
+	{
+		CalculaterTest cal= new CalculaterTest();
+		int a=7;
+		int b=10;
+		int expres=17;
+		int actres=cal.add(a,b);
+		if(actres==expres)
+		{
+			System.out.println("pass");
+		}
+		else
+			System.out.println("Fail");
+		
+	}
+
+	
+	public void mulTest()
+	{
+		CalculaterTest cal= new CalculaterTest();
+		int a=7;
+		int b=10;
+		int expres=70;
+		int actres=cal.multi(a,b);
+		if(actres==expres)
+		{
+			System.out.println("pass");
+		}
+		else
+			System.out.println("Fail");
+		
 	}
 
 	
