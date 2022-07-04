bff1d5485628369fa286c1996ddd1608f0eedf9e
VimeeBakori <vimeebakori@gmail.com>
2022-07-04 11:38:14 +0200

    Multiplacation code added by developer

--- GitDemo/src/testGit/CalculaterTest.java
+++ GitDemo/src/testGit/CalculaterTest.java
@@ -1,10 +1,15 @@
 package testGit;
 
-public class Add {
+public class CalculaterTest {
 	public static int add(int i, int j) {
 		// TODO Auto-generated method stub
 		 int c=i+j;
 		return c;
+	}
+	public static int multi(int i, int j) {
+		// TODO Auto-generated method stub
+		
+		return i*j;
 	}
 
 	
